1618542cccf4431657f57fa770e9457c0e2d3061
Jason Yue <kymotsujason@live.com>
2018-11-07 22:40:51 -0800

    Clean up read-me and add link to github
    - Moved create-react-app readme to another file (cleaner)
    - Added link to this git next to footer

--- src/generic_components/views/Footer.jsx
+++ src/generic_components/views/Footer.jsx
@@ -9,7 +9,8 @@ class Footer extends Component {
 				<div className="center_text footer_bar">
 					<div className="p-col-1">
 					</div>
-					<p>@2018 Jason Yue - Powered by Django and React</p>
+					<p>@2018 Jason Yue - Powered by Django and React <a href="https://github.com/kymotsujason/SomeModernSite">(Github)</a></p>
+					
 					<Time />
 					<div className="p-col-1">
 					</div>
